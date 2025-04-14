2d75e5e2145dfe4f6bf76a6e2c778175f341c855
nicolas-goyon <38950115+nicolas-goyon@users.noreply.github.com>
2025-04-14 12:43:44 +0000

    1st step into dependencies - before breaking parsing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CodeToClassDiagram',
-    version='0.1.0',
+    version='1.0.2',
     description='Génération de diagrammes de classes à partir d’un projet source.',
     author='Votre Nom',
     packages=find_packages(),
